c5ba38cffcf8a705ab888b75549fbb11b25293b5
WangLabTHU <66990292+WangLabTHU@users.noreply.github.com>
2021-01-30 15:50:09 +0800

    Update Fountain_analyzer.py

--- Fountain_analyzer.py
+++ Fountain_analyzer.py
@@ -1,9 +1,10 @@
 from Encode.Helper_Functions import dna_to_int_array, rs_decode, preprocess, load_dna
-from Analysis.Analysis import dna_chunk, save_simu_result
+from Analysis.Analysis import dna_chunk, save_simu_result,  error_distribution
 from Encode.DNAFountain import DNAFountain, Glass
 import numpy as np
 from scipy.stats import gumbel_r, poisson
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def error_profile(out_dnas, rs = 2):
     lost_num = 0
@@ -12,7 +13,7 @@ def error_profile(out_dnas, rs = 2):
     ignore_index = []
     for i,dna in enumerate(out_dnas):
         
-        if dna['num'] is 0: 
+        if dna['num'] == 0: 
             lost_num += 1
             continue
         
@@ -98,7 +99,9 @@ class FT_Analyzer:
         NP = np.mean(self.loss_nums) + np.mean(self.fail_nums)
         self.p = NP / (1+self.alpha) / self.chunk_num
         self.loc, self.scale = gumbel_r.fit(self.decode_lines)
-        
+    
+
+
     def fail_prob(self,alpha,plot = False, show_E = True):
         self.compute_dist()
         X = np.arange(0,int(alpha * self.chunk_num))
@@ -152,6 +155,119 @@ class FT_Analyzer:
         
         return p_fail_list
 
+
+class FT_Analyzer_Simplified:
+    def __init__(self, N, Ld, alpha, loc, scale, seq_dnas):
+        self.alpha = alpha
+        self.N = N
+        self.Ld = Ld
+
+        self.loc = loc
+        self.scale = scale
+
+        self.seq_dnas = seq_dnas
+
+    def choose_rs(self):
+        # obtain Nl, Ne(i) from seq_dnas:
+        rNs = [dna['num'] for dna in self.seq_dnas]
+        Nl = sum([rN == 0 for rN in rNs])
+        Nd = np.array(error_distribution(self.seq_dnas))
+        Ne = [np.sum(Nd == i) for i in range(20)]
+        while Ne[-1] == 0:
+            Ne = Ne[:-2]
+        # compute D(k)
+        K = [i for i in range(2,len(Ne))]
+        Ne = Ne + [0]
+        D = []
+        for k in K:
+            sumEN = sum(Ne[k+1:])
+            Dk = self.Ld / (self.Ld + 2 * k) * self.N / (self.loc + Nl + sumEN)
+            D.append(Dk)
+        # choose RS
+        maxD = max(D)
+        i = D.index(maxD)
+        k = K[i]
+        self.Ntl = sum(Ne[k+1:]) + Nl
+        self.rs = k * 2
+        self.p = self.Ntl / (1+self.alpha) / self.N
+        # plot 
+        fig = plt.figure(figsize = (12,6))
+        plt.subplot(1,2,1)
+        ax = sns.distplot(Nd, hist= False, color="r", kde_kws={"shade": True, 'bw': 0.1})
+        plt.xlabel('error number')
+        plt.ylabel('frequency')
+        plt.subplot(1,2,2)
+        sns.lineplot(x = (np.array(K) * 2)[:len(D)], y = D, color = "r")
+        plt.xlabel('RS length')
+        plt.ylabel('Estimated information density')
+        
+        return fig, self.rs
+
+    def choose_alpha(self):
+        fig = plt.figure(figsize = (12,6))
+        plt.subplot(1,2,1)
+        self.fail_prob(self.alpha-0.05, plot = True)
+        self.fail_prob(self.alpha, plot = True, show_E = False)
+        self.fail_prob(self.alpha+0.05, plot = True, show_E = False)
+        plt.subplot(1,2,2)
+        self.alpha_scan()
+        return fig
+        
+    
+
+    def fail_prob(self,alpha,plot = False, show_E = True):
+        X = np.arange(0,int(alpha * self.N))
+        
+        X_decode = int((1+alpha) * self.N) - X
+        P_correct = gumbel_r.pdf(X_decode,self.loc, self.scale)
+        P_correct = P_correct / np.sum(P_correct)
+
+        u_error = int((1+alpha) * self.N) * self.p
+        P_error = poisson.pmf(X,u_error)
+        
+        p_fail_to_correct = 0
+        for i in X:
+            p_fail_to_correct += P_error[i] * np.sum(P_correct[:i+1])
+
+        if plot: 
+            no_zeros = np.where( (P_correct + P_error) > 0.00001)
+            s,e = np.min(no_zeros), np.max(no_zeros)
+            X = X[s:e]
+            P_error = P_error[s:e]
+            P_correct = P_correct[s:e]
+            if show_E:
+                plt.plot(X,P_error,color = 'red',label = f'generated losts')
+                plt.fill_between(X,0,P_error,alpha = 0.3, color = 'red')
+
+            plt.plot(X,P_correct,color = 'blue',label = f'alpha = {round(alpha,2)}\nfail prob = {round(p_fail_to_correct*100,2)}%')
+            plt.fill_between(X,0,P_correct,alpha = 0.3, color = 'blue')
+
+            plt.legend()
+            # plt.show()
+
+        return p_fail_to_correct
+    
+    def alpha_scan(self, alpha_list = None, plot = True, points = 15, color = 'black'):
+        if not alpha_list:
+            alpha_list = np.linspace(self.alpha-0.15,self.alpha+0.1,points)
+        p_fail_list = []
+        # info_ratio_list = []
+        for alpha in alpha_list:
+            p_fail = self.fail_prob(alpha)
+            # info_ratio = round(self.chunk_size/(self.rs_length + self.chunk_size)/(1+alpha),4)
+            p_fail_list.append(p_fail) 
+            # info_ratio_list.append(info_ratio)
+        for i in range(len(p_fail_list)):
+            p_fail_list[i] = max(p_fail_list[i:])
+
+        if plot:
+            plt.plot(alpha_list,p_fail_list,label = 'fail prob',color = color)
+            plt.fill_between(alpha_list,0,p_fail_list,alpha = 0.3, color = color)
+            plt.xlabel('Alpha')
+            # plt.plot(alpha_list,info_ratio_list,label = 'information density')
+            plt.legend()
+        
+        return p_fail_list
         
 
 
